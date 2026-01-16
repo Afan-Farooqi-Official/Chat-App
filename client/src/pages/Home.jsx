54d2de949e09ff01f2aeb6962498011f342fe29e
Afan-Farooqi-Official <affanqaisar000@gmail.com>
2026-01-16 22:18:55 +0500

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import RightSidebar from '../components/RightSidebar'
+
+const Home = () => {
+
+  const [selectUser, setSelectUser] = useState(null)
+
+  return (
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+      <div className={`backdrop-blur-sm border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
+        <Sidebar selectedUser={selectUser} setSelectUser={setSelectUser} />
+        <ChatContainer />
+        <RightSidebar />
+      </div>
+    </div>
+  )
+}
+
+export default Home
