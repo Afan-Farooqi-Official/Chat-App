205bf23928c1b950487646928a2b72ad793cf409
Afan-Farooqi-Official <affanqaisar000@gmail.com>
2026-01-17 15:40:42 +0500

    frontend complete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,8 +11,8 @@ const Home = () => {
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
       <div className={`backdrop-blur-sm border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
         <Sidebar selectedUser={selectUser} setSelectUser={setSelectUser} />
-        <ChatContainer />
-        <RightSidebar />
+        <ChatContainer selectedUser={selectUser} setSelectUser={setSelectUser} />
+        <RightSidebar selectedUser={selectUser} setSelectUser={setSelectUser} />
       </div>
     </div>
   )
